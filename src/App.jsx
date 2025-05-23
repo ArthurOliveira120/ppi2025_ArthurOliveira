8f0bdd6feb0c6bd8b31b21d7aacf64229c2cfc45
ArthurOliveira120 <arthuromarinho10@gmail.com>
2025-05-23 18:54:34 +0000

    Aula 23/05/25 - Props, children e rendenização usando maps

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,27 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText";
 
 export default function App() {
+  const texts = [
+    {
+      title: "Título ",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Título ",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Título ",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+  ];
+
   return (
+    //React Fragment
     <>
-      <MyText />
-      <MyText/>
+      {texts.map((item, index) => {
+        return <MyText title={`${index+1}. ${item.title}`}> {item.text} </MyText>;
+      })}
     </>
   );
 }
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,16 +1,11 @@
 import styles from "./MyText.module.css";
 
-export function MyText() {
+export function MyText({ title, children }) {
   return (
     <div className={styles.container}>
       <div className={styles.div}>
-        <h1 className={styles.title}>Meu primeiro React App</h1>
-        <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae quisquam
-          modi consequatur eum, consequuntur aliquid repellendus iure quas
-          exercitationem harum debitis atque, suscipit, ducimus dignissimos
-          necessitatibus beatae minima dolorum quia.
-        </p>
+        <h1 className={styles.title}>{title}</h1>
+        <p className={styles.text}>{children}</p>
       </div>
     </div>
   );
