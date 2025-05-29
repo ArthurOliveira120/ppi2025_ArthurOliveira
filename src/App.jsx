96f9701b2f982b3fd4d41a758ccaf0b271a7c0b0
ArthurOliveira120 <arthuromarinho10@gmail.com>
2025-05-29 19:08:54 +0000

    Aula 29/05 - CSS Flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { MyText } from "./components/MyText";
+import { MyTextList } from "./components/MyTextList";
 
 export default function App() {
-  const texts = [
-    {
-      title: "Título ",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      title: "Título ",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      title: "Título ",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    },
-  ];
+  
 
   return (
     //React Fragment
     <>
-      {texts.map((item, index) => {
-        return <MyText title={`${index+1}. ${item.title}`}> {item.text} </MyText>;
-      })}
+      <MyTextList></MyTextList>
     </>
   );
 }
---
+++ src/components/MyTextList.jsx
@@ -0,0 +1,37 @@
+import styles from "./MyTextList.module.css";
+import { MyText } from "./MyText";
+
+export function MyTextList() {
+  const texts = [
+    {
+      title: "Título",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Título",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Título",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Título",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    },
+    {
+      title: "Título",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    }
+  ];
+
+  return (
+    <div className={styles.container}>
+      {texts.map((item, index) => (
+        <MyText title={`${index + 1}. ${item.title}`}>
+            {item.text}
+        </MyText>
+      ))}
+    </div>
+  );
+}
