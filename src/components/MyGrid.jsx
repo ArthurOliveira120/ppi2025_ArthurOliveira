2b833851c20e079597b6cf74350da9633ccb6cad
ArthurOliveira120 <arthuromarinho10@gmail.com>
2025-06-08 18:35:24 +0000

    Desafio 3

--- src/components/MyGrid.jsx
+++ src/components/MyGrid.jsx
@@ -3,38 +3,40 @@ import styles from "./MyGrid.module.css";
 export function MyGrid() {
   return (
     <div className={styles.container}>
-        <header className={styles.header1}/>
-        <header className={styles.header2}></header>
-        <aside className={styles.aside}></aside>
-      <div className={styles.main}>
-        <div className={styles.grid}>
-          <div className={styles.card}>
-            <h2>card 1</h2>
-            <p>this is a card</p>
-          </div>
+      <header className={styles.header1} />
+      <header className={styles.header2} />
+      <aside className={styles.aside1} />
+      <aside className={styles.aside2} />
 
-          <div className={styles.card}>
-            <h2>card 2</h2>
-            <p>this is a card</p>
-          </div>
+      <div className={styles.grid}>
+        <div className={styles.card}>
+          <h2>card 1</h2>
+          <p>this is a card</p>
+        </div>
 
-          <div className={styles.card}>
-            <h2>card 3</h2>
-            <p>this is a card</p>
-          </div>
+        <div className={styles.card}>
+          <h2>card 2</h2>
+          <p>this is a card</p>
+        </div>
 
-          <div className={styles.card}>
-            <h2>card 4</h2>
-            <p>this is a card</p>
-          </div>
+        <div className={styles.card}>
+          <h2>card 3</h2>
+          <p>this is a card</p>
+        </div>
 
-          <div className={styles.card}>
-            <h2>card 5</h2>
-            <p>this is a card</p>
-          </div>
+        <div className={styles.card}>
+          <h2>card 4</h2>
+          <p>this is a card</p>
+        </div>
+
+        <div className={styles.card}>
+          <h2>card 5</h2>
+          <p>this is a card</p>
         </div>
       </div>
-      <footer className={styles.footer}></footer>
+
+      <footer className={styles.footer2}></footer>
+      <footer className={styles.footer1}></footer>
     </div>
   );
 }
